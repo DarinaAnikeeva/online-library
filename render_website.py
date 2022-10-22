a9a794eb2ed99ecd11a418a223399307e5b4ce94
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2022-10-22 15:29:40 +0300

    Update render_website.py

--- render_website.py
+++ render_website.py
@@ -7,8 +7,7 @@ from more_itertools import chunked
 
 def on_reload():
     with open("books_params.json", 'r', encoding='utf-8') as file:
-        books_params = file.read()
-        params_dicts = json.loads(books_params)
+        params_dicts = json.load(file)
 
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -18,24 +17,27 @@ def on_reload():
     template = env.get_template('template.html')
 
     os.makedirs('pages',  exist_ok=True)
-    books_pages = list(chunked(params_dicts, 15))
+    quantity_books_in_page = 15
+    books_pages = list(chunked(params_dicts, quantity_books_in_page))
 
-    for number, books_page in enumerate(books_pages):
-        book_params = list(chunked(books_page, 2))
+    for number, books_page in enumerate(books_pages, start=1):
+        quantity_books_in_raw = 2
+        books_params = list(chunked(books_page, quantity_books_in_raw))
         rendered_page = template.render(
-            books_params=book_params,
+            books_params=books_params,
             books_pages=len(books_pages),
-            page_number=number+1
+            page_number=number
         )
 
-        with open(f'pages\index{number+1}.html', 'w', encoding="utf8") as file:
+        with open(f'pages\index{number}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
-on_reload()
+if __name__ == '__main__':
+    on_reload()
 
-server = Server()
-server.watch('template.html', on_reload)
-server.watch('books_params.json', on_reload)
-server.serve(root='.')
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.watch('books_params.json', on_reload)
+    server.serve(root='.')
 
 
